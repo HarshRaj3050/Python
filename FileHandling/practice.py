3f40d1670d5172be0a07883b7cdf3596d1add7ec
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-15 22:19:19 +0530

    practice

--- FileHandling/practice.py
+++ FileHandling/practice.py
@@ -1,9 +1,3 @@
 ''' Write a program to generate multiplication table from 2 to 20 and write it
 to the differend files. place these files in a folder for a 13 years old '''
 
-for i in range(2, 21):
-    with open(f"table/tables_of_{i}.txt", 'w') as f:
-        for j in range(1,11):
-            f.write(f"{i} X {j} = {i*j}")
-            if(j!=10):
-                f.write('\n')
