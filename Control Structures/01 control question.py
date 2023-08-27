88e5ad50b154b6411c7ffc701d937f53c0b89ae6
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-27 23:47:36 +0530

    update

--- Control Structures/01 control question.py
+++ Control Structures/01 control question.py
@@ -1,3 +1,8 @@
 # Write a python program to compare two numbers using nested condition.
 
-
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+if(a > b):
+    print("First Number is greater than Second Number")
+else:
+    print("Second Number is greater than First Number")
