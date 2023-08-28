2fde9ae2c7279253f04533412257e321ff1fdf85
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-28 23:50:40 +0530

    update

---
+++ Control Structures/02 control question.py
@@ -0,0 +1,9 @@
+''' Write a python program to display the result such as distinction, first class,
+    second class, pass or fail based on the marks entered by the user. '''
+
+print('''----Result----
+Marks 81-100 = First class
+Marks 61-80 = Second class
+Marks 41-60 = Pass
+Marks 00-40 = Fail''')
+
