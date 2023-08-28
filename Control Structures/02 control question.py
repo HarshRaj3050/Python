4864dc3603a831f5a451ae5ff69ae1f6b94926a4
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-29 00:03:11 +0530

    update

--- Control Structures/02 control question.py
+++ Control Structures/02 control question.py
@@ -8,11 +8,13 @@ Marks 41-60 = Pass
 Marks 00-40 = Fail\n\n''')
 
 marks = int(input("Enter your Marks : "))
-if(marks >= 81 or marks <= 100):
-    print("First Class")
-elif(marks >= 61 or marks <= 80):
-    print("Second Class")
-elif(marks >= 41 or marks <= 60):
-    print("Pass")
-elif(marks >= 0 or marks <= 40):
-    print("Fail")
+if(marks >= 81 and marks <= 100):
+    print("Your marks is : First Class")
+elif(marks >= 61 and marks <= 80):
+    print("Your marks is : Second Class")
+elif(marks >= 41 and marks <= 60):
+    print("Your marks is : Pass")
+elif(marks >= 0 and marks <= 40):
+    print("Your marks is : Fail")
+else:
+    print("Enter Valid marks")
