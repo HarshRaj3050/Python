d5f3e6118328584ecbb62b6e0664d0a7d916904a
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-29 23:56:32 +0530

    update

--- Loop/01 loop question.py
+++ Loop/01 loop question.py
@@ -1,2 +1,13 @@
 ''' Write a python program for computing the sum of n natural numbers and 
     numbers and finding out the average of it. '''
+
+num = int(input("Enter the Number : "))
+sum = 0
+i = 1
+while(i <= num):
+    sum = sum + i
+    i = i+1
+avg = sum % num
+print(f"The sum of {num} Natural number is : {sum}")
+print(f"The average of {num} Natural number is : {avg}")
+
