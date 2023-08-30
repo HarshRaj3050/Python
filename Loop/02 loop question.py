73b28068d338b492c6e6a51c391d5dca7e692a1d
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-30 23:53:14 +0530

    update

---
+++ Loop/02 loop question.py
@@ -0,0 +1,8 @@
+# Write a python program to display square of n number using while loop.F
+
+num = int(input("Enter the Number : "))
+i = 1
+while(i<=num):
+    square = i*i
+    i = i+1
+print(f"Square root of {num} is : {square}")
