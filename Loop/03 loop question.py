fb94fbc67fbd7b287d43a2dbe8288f2e980f7115
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-31 23:38:58 +0530

    update

---
+++ Loop/03 loop question.py
@@ -0,0 +1,10 @@
+num = int(print("enter the number : "))
+i = 1
+j = 1
+while i<=num:
+    if j%2 == 0:
+        print(f"{j} is Even")
+    else:
+        print(f"{j} is Odd")
+    i = i + 1
+    j = i
