1c78daace77f1294e82574cc9fa6ae153e06101f
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-12 23:56:13 +0530

    practice

--- Function/practice.py
+++ Function/practice.py
@@ -1,10 +1,13 @@
 # Find the Factorial of any n number using recursion.
 
 def factorial(n):
-    if(n == 0 or n == 1):
-        return 1
-    else:
-        return n * factorial(n-1)
+    i = 1
+    fact = 1
+    while(i <= n):
+        fact = fact * i
+        i = i + 1
+    return fact
+
 num = int(input("Enter the number : "))
 fact = factorial(num)
 print(f"Factorial of {num} is : {fact}")
