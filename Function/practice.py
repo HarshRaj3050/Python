4acb0180b14580853869396c15be1ca4bf49ceb3
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-13 23:53:05 +0530

    practice

--- Function/practice.py
+++ Function/practice.py
@@ -1,13 +1,1 @@
-# Find the Factorial of any n number using recursion.
-
-def factorial(n):
-    i = 1
-    fact = 1
-    while(i <= n):
-        fact = fact * i
-        i = i + 1
-    return fact
-
-num = int(input("Enter the number : "))
-fact = factorial(num)
-print(f"Factorial of {num} is : {fact}")
+# Write a python program using function to check if the enered year is leap year or not.
