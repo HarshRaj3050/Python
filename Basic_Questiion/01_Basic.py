a4499b27913a9d981d7f1652503fd6c978b34be8
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-14 23:48:06 +0530

    update

--- Basic_Questiion/01_Basic.py
+++ Basic_Questiion/01_Basic.py
@@ -1,4 +1,14 @@
 # To swap the value of two variables  
 # we will user third variable which is a temporary variable 
 
+a = int(input("Enter the number : "))
+b = int(input("Enter the number : "))
 
+print("Before Swap")
+print(f"a = {a}, b = {b}")
+temp = a
+a = b
+b = temp
+
+print("\nAfter swap")
+print(f"a = {a}, b = {b}")
