5b177932130b7645fcb68d70b4cfd25582adaf94
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-16 23:46:42 +0530

    update

--- Basic_Questiion/02 basic question.py
+++ Basic_Questiion/02 basic question.py
@@ -1,3 +1,13 @@
-# To swap the value of two variables  
-# We can use the comma operator. 
+# To swap the value of two variables. We can use the comma operator. 
 # We do not need to use a third variable for swapping the values of two variables for this method
+
+a = int(input("Enter the number : "))
+b = int(input("Enter the number : "))
+
+print("Before Swap")
+print(f"a = {a}, b = {b}")
+
+a, b = b, a
+
+print("\nAfter swap")
+print(f"a = {a}, b = {b}")
