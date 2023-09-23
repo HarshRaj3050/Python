ac7afc28700bc50a53d0e18374984c963e4ea072
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-23 23:43:21 +0530

    practice

---
+++ Basic_Questiion/practice.py
@@ -0,0 +1,9 @@
+# To swap the value of two variables. We can use the comma operator. 
+# We do not need to use a third variable for swapping the values of two variables for this method
+
+a = 62
+b = 29
+
+print(f"a = {a} , b = {b}")
+a, b = b, a
+print(f"a = {a} , b = {b}")
