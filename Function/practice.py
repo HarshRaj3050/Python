1677a0002249d42c0db7461890ac0f66ba128926
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-24 23:38:20 +0530

    practice

--- Function/practice.py
+++ Function/practice.py
@@ -1,1 +1,14 @@
 # Write a python program using function to check if the enered year is leap year or not.
+
+def leap(year):
+    if(year % 4 == 0 and year % 100 != 0):
+        print("The Year is leap year")
+    elif(year % 400 == 0):
+        print("The year is leap year")
+    elif(year % 100 == 0):
+        print("The year is not leap year")
+    else:
+        print("The is Year is not leap year")
+
+year = int(input("Enter the Year : "))
+leap(year)
